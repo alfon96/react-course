12941148890fe3af6318409b079be6a80288b93f
Alfonso Falcone <falcone@spici.eu>
2023-09-12 14:49:17 +0200

    add modal component

--- food_order_app/src/components/Meals/AvailableMeals.js
+++ food_order_app/src/components/Meals/AvailableMeals.js
@@ -31,7 +31,13 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} name={meal.name} price={meal.price} />
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      description={meal.description}
+      name={meal.name}
+      price={meal.price}
+    />
   ));
   return (
     <section className={classes.meals}>
