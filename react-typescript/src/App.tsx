b7f3f2f13d43088a215e8359f27adbd0fb34c23d
alfon96 <alfalcone1996@gmail.com>
2023-09-28 18:11:42 +0200

    add props in ts

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <Todos items={['Learn React','Learn Typescript']}/>
+    </div>
+  );
+}
+
+export default App;
